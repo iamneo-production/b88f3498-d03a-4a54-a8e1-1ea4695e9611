051309251e54e6a7224be27d7c04715761f0759c
darshangoli <satyadarshan4022@gmail.com>
2023-06-28 15:46:44 +0000

    created dashboard with small issues in css.

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WorkoutplanComponent } from './workoutplan/workoutplan.component';
+import { GoalsettingComponent} from './goalsetting/goalsetting.component';
+import { RecommendationsComponent } from './recommendations/recommendations.component';
+import { WorkouthistoryComponent } from './workouthistory/workouthistory.component';
+import { ExercisetrackingComponent } from './exercisetracking/exercisetracking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'workout', component: WorkoutplanComponent},
+  { path: 'exertracking', component: ExercisetrackingComponent},
+  { path: 'goalsetting', component: GoalsettingComponent},
+  { path: 'recommendations', component: RecommendationsComponent},
+  { path: 'history', component: WorkouthistoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angularapp/src/app/sidenav/sidenav.component.ts
+++ angularapp/src/app/sidenav/sidenav.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent {
 
