605076d9969f251521a50e7bf819c66c5cd75ac5
vermavivek951 <vermavivek951@gmail.com>
2023-06-29 14:34:56 +0000

    code cleanup

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -22,12 +22,8 @@ import { ContainerComponent } from './components/container/container.component';
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegistrationComponent } from './components/registration/registration.component';
-<<<<<<< HEAD
-import { SuriyaComponent } from './suriya/suriya.component';
 import { CustomNutritionRecomendationComponent } from './components/recommendations/custom-nutrition-recomendation/custom-nutrition-recomendation.component';
-=======
 import { GoalSettingComponent } from './components/goal-setting/goal-setting.component';
->>>>>>> a978b8f9bbbe58a909423e450545f778df239390
 
 @NgModule({
   declarations: [
@@ -43,12 +39,8 @@ import { GoalSettingComponent } from './components/goal-setting/goal-setting.com
     ContainerComponent,
     RegistrationComponent,
     LoginComponent,
-<<<<<<< HEAD
-    SuriyaComponent,
-    CustomNutritionRecomendationComponent
-=======
+    CustomNutritionRecomendationComponent,
     WorkoutplanComponent,
->>>>>>> a978b8f9bbbe58a909423e450545f778df239390
   ],
   imports: [
     BrowserModule,
