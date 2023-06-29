dce57672bd8f35cdbd163711f98e34fc1d4ded9e
vermavivek951 <vermavivek951@gmail.com>
2023-06-29 15:06:32 +0000

    code cleanup

--- angularapp/src/app/components/error-page/error-page.component.ts
+++ angularapp/src/app/components/error-page/error-page.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { TitleService } from '../../services/title.service';
-=======
 import { TitleService } from 'src/app/services/title.service';
->>>>>>> a978b8f9bbbe58a909423e450545f778df239390
 
 @Component({
   selector: 'app-error-page',
