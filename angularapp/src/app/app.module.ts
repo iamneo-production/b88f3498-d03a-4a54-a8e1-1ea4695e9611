d74737202cf8f54dc1968f7766d95cfdc436c116
darshangoli <satyadarshan4022@gmail.com>
2023-06-29 18:46:10 +0000

    added exercise component and updated whole app

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -28,6 +28,9 @@ import { MatTableModule } from '@angular/material/table';
 import { WorkoutplanComponent } from './components/workoutplan/workoutplan.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GoalSettingComponent } from './components/goal-setting/goal-setting.component';
+import { GoaltrackingComponent } from './components/goaltracking/goaltracking.component';
+import { HtwtcompComponent } from './components/htwtcomp/htwtcomp.component';
+import { DatetrackingComponent } from './components/datetracking/datetracking.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +46,10 @@ import { GoalSettingComponent } from './components/goal-setting/goal-setting.com
     LoginComponent,
     CustomNutritionRecomendationComponent,
     WorkoutplanComponent,
-    GoalSettingComponent  
+    GoalSettingComponent,
+    GoaltrackingComponent,
+    HtwtcompComponent,
+    DatetrackingComponent  
 
   ],
   imports: [
