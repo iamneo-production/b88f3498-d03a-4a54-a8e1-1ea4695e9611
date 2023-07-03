59aa68198104bc13bde09d0f35adb44fbd0aa973
mathushree <mathushree55@gmail.com>
2023-07-03 14:04:35 +0000

    getAllWorkout test

--- springapp/src/main/java/com/example/springapp/controller/WorkoutController.java
+++ springapp/src/main/java/com/example/springapp/controller/WorkoutController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springapp.repository.UserRepository;
 import com.example.springapp.repository.WorkoutRepository;
 
 @RestController
@@ -12,14 +13,25 @@ import com.example.springapp.repository.WorkoutRepository;
 @RequestMapping("/workout")
 public class WorkoutController {
 
-    // @Autowired
-    // private WorkoutRepository workoutRepository;
+    @Autowired
+    private WorkoutRepository workoutRepository;
 
-    // @GetMapping
-    // public List<Workout> getAllWorkout(){
-    // return (List<Workout>) workoutRepository.findAll();
+    @Autowired
+    private UserRepository userRepository;
 
-    // }
+    @GetMapping
+    public Iterable<Workout> getAllWorkout(){
+        Iterable<Workout> allWorkouts = workoutRepository.findAll();
+        Iterator iterator = allWorkouts.iterator();
+        while(iterator.hasNext()){
+            Workout workout = iterator.next();
+            User user = workout.getUser();
+            int userId = user.getId();
+            User userFromUsersTable = userRepository.getUserById(userId);
+            workout.setUser(userFromUsersTable);
+        }
+        return allWorkouts;
+    }
 
     // @GetMapping("/id")
     // public Workout getWorkoutById(long id){
