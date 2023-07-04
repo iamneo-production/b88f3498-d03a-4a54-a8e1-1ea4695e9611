75809ce19bb29fbfe4b91bf9a4e860e07ff45186
vermavivek951 <vermavivek951@gmail.com>
2023-07-04 06:16:54 +0000

    cross origin setup

--- springapp/src/main/java/com/example/springapp/controller/UserController.java
+++ springapp/src/main/java/com/example/springapp/controller/UserController.java
@@ -14,13 +14,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.springapp.model.User;
-import com.example.springapp.service.impl.UserService;
+import com.example.springapp.service.UserService;
 
 
 @RestController
-// @CrossOrigin(origins = "*")
+@CrossOrigin
 @RequestMapping("/user")
 public class UserController {
 
