60e4ff57c3418335b299ca847d975f7de3b46c57
vermavivek951 <vermavivek951@gmail.com>
2023-07-05 02:51:27 +0000

    workoutservice and setservice

--- springapp/src/main/java/com/example/springapp/service/WorkoutService.java
+++ springapp/src/main/java/com/example/springapp/service/WorkoutService.java
@@ -1,31 +1,3 @@
-package com.example.springapp.service.impl;
-
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import com.example.springapp.model.Workout;
-import com.example.springapp.service.WorkoutServiceInterface;
-
-
-@Service
-public class WorkoutService implements WorkoutServiceInterface{
-    @Override
-    public List<Workout> getAllWorkout(){
-        return null;
-
-    }
-    @Override
-     public Workout getWorkoutById(long id){
-        return null;
-     }
-     @Override
-     public List<Workout> getWorkOutByUserId(long user_id){
-        return null;
-        
-     } 
-    
-}
 package com.example.springapp.service;
 
 import java.util.List;
