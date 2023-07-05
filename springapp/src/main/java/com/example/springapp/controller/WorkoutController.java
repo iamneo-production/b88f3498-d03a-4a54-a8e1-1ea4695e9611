2469bec020e1a351d309b57cd6c88ab200e61e06
mathushree <mathushree55@gmail.com>
2023-07-05 06:57:48 +0000

    Added New Method in WorkoutService Impl and interface

--- springapp/src/main/java/com/example/springapp/controller/WorkoutController.java
+++ springapp/src/main/java/com/example/springapp/controller/WorkoutController.java
@@ -1,6 +1,8 @@
 package com.example.springapp.controller;
 
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -18,17 +20,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Set;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import springapp.springapp.model.Set;
+import springapp.springapp.model.User;
+import springapp.springapp.model.Workout;
+import springapp.springapp.repository.UserRepository;
+import springapp.springapp.repository.WorkoutRepository;
+import springapp.springapp.service.impl.WorkoutService;
 
-import com.example.springapp.model.User;
-import com.example.springapp.model.Workout;
-import com.example.springapp.repository.UserRepository;
-import com.example.springapp.service.WorkoutService;
-import com.example.springapp.repository.WorkoutRepository;
 @RestController
-@RequestMapping("/workout")
 @CrossOrigin(origins = "https://8081-bfbbcbbafccbbbdaaaccdcddcffebdffccbebc.project.examly.io")
+@RequestMapping("/workout")
 public class WorkoutController {
 
     @Autowired
@@ -39,23 +43,11 @@ public class WorkoutController {
 
     @Autowired
     private UserRepository userRepository;
-   
-    /**
-     * @param workout
-     * @return
-     */
+    
+    
     @PostMapping
     public ResponseEntity<String> createWorkout(@RequestBody Workout workout) {
-        long userId = workout.getUser().getId();
-        User newUser = new User();
-        newUser.setId(userId);
-        workout.setUser(newUser);
-        Workout createdWorkout = workoutRepository.save(workout);
-        if (createdWorkout != null) {
-            return ResponseEntity.ok("Workout Created");
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        return workoutService.addOrUpdateWorkout(workout, "Workout Created");
     }
      
     @GetMapping
@@ -63,30 +55,20 @@ public class WorkoutController {
         Iterable<Workout> workouts = workoutRepository.findAll();
         return ResponseEntity.ok(workouts);
     }
-
+    
     @GetMapping("/id")
     public Workout getWorkoutById(@RequestParam("id") long id){
         return workoutService.getWorkoutById(id);
     }
-    
+
     @GetMapping("/userid")
     public List<Workout> getWorkoutByUserId(@RequestParam("userid") long id){
         return workoutService.getWorkOutByUserId(id);
     }
 
-    @PutMapping("/user")
+    @PutMapping
     public ResponseEntity<String> updateWorkout(@RequestBody Workout workout) {
-        long userId = workout.getUser().getId();
-        User newUser = new User();
-        newUser.setId(userId);
-        workout.setUser(newUser);
-        Workout createdWorkout = workoutRepository.save(workout);
-        if (createdWorkout != null) {
-            return ResponseEntity.ok("Workout Updated");
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        return workoutService.addOrUpdateWorkout(workout, "Workout Updated");
     }
 }
-
 
