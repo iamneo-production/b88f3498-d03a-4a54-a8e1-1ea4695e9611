1b5341695fb0d839d42ed777c45150ef44660d45
SuriyaG894 <suriyag894@gmail.com>
2023-07-05 06:27:47 +0000

    setcontroller added

---
+++ springapp/src/main/java/com/example/springapp/controller/SetController.java
@@ -0,0 +1,47 @@
+package main.java.com.example.springapp.controller;
+
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.example.springapp.model.Set;
+import com.example.springapp.model.Exercise;
+import com.example.springapp.service.SetService;
+import com.example.springapp.repository.SetRepository;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/set")
+public class SetController {
+
+    @Autowired
+    private SetService setService;
+
+    @GetMapping
+    public Iterable<Exercise> getAllSet() {
+        return setService.getAllSet();
+    }
+    @GetMapping("/id")
+    public Exercise getSetById(@RequestParam("id") long id){
+        return setService.getSetById(id);
+    }
+
+    @DeleteMapping("/id")
+    public void deleteSetById(@RequestParam("id") long id){
+         setService.getSetById(id);
+    }
+
+}
