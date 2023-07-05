62921b8792a821b487a19664609e2ac9d2d51ea5
SuriyaG894 <suriyag894@gmail.com>
2023-07-05 05:46:34 +0000

    .

--- springapp/src/main/java/com/example/springapp/controller/ExerciseController.java
+++ springapp/src/main/java/com/example/springapp/controller/ExerciseController.java
@@ -10,18 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;import com.example.springapp.service.ExerciseService;
 
-
+@CrossOrigin(origins = "https://8081-bfbbcbbafccbbbdaaaccdcddcffebdffccbebc.project.examly.io")
 @RestController
 public class ExerciseController {
 
     @Autowired()
     private ExerciseServiceInterface exerciseServiceInterface;
 
-
-    @GetMapping("/")
-    public String hello(){
-        return "Hello World";
-    }
 
     @GetMapping("/exercise")
     public ResponseEntity<List<Exercise>> getAllExercises(){
