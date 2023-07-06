8f7dc2a6c4f16952bb619738a89d919d5d9ca568
vermavivek951 <vermavivek951@gmail.com>
2023-07-06 07:11:43 +0000

    password validation error solved and touched UI of reg page

--- angularapp/src/app/services/form-validation.service.ts
+++ angularapp/src/app/services/form-validation.service.ts
@@ -10,7 +10,7 @@ export class FormValidationService {
   myform = new FormGroup({
     email : new FormControl('', [Validators.required, Validators.email]),
     name : new FormControl('', [Validators.required, Validators.minLength(3)]),
-    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/), Validators.minLength(8)]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.$!%*?&])[A-Za-z\d@.$!%*?&]+$/), Validators.minLength(8)]),
     height : new FormControl('', [Validators.required]),
     weight : new FormControl('', [Validators.required]),
     age : new FormControl('', [Validators.required]),
