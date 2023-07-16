19c5a32bca0c8ee3bf1b596ac06bff5a13678008
vermavivek951 <vermavivek951@gmail.com>
2023-07-16 14:31:51 +0000

    code cleanup

--- springapp/src/main/java/com/example/springapp/controller/HtwtTrackingController.java
+++ springapp/src/main/java/com/example/springapp/controller/HtwtTrackingController.java
@@ -20,7 +20,7 @@ import com.example.springapp.repository.HtwtTrackingRepository;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin( origins="https://8081-bfbbcbbafccbbbdaaaccdcddcffebdffccbebc.project.examly.io", maxAge = 3600, allowCredentials="true")
+@CrossOrigin( origins="https://8081-cedaebaacbebdefdaaaccdcddcffebdffccbebc.project.examly.io", maxAge = 3600, allowCredentials="true")
 public class HtwtTrackingController {
 
     @Autowired
