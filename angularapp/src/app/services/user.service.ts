226f9027155ed318b1af1c4245e3feefdc6dce84
vermavivek951 <vermavivek951@gmail.com>
2023-07-18 15:36:03 +0000

    code cleanup

--- angularapp/src/app/services/user.service.ts
+++ angularapp/src/app/services/user.service.ts
@@ -15,8 +15,10 @@ export class UserService implements OnDestroy {
     this.httpClient = httpClient;
     this.tokenService = tokenService;
   }
+
   baseUrl:string = environment.baseUrl;
   user: any = {id:0, email: '',  name: 'DefaultUser', password: '', height: 67, weight: 56, age: 20, gender: 'Female', imagePath:  "./../../../assets/icon/user.png" };
+
   userCalorie: number = 2000;
   userSubject = new BehaviorSubject(this.user);
 
