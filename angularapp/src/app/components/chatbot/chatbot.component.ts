8e3ffcd5efe1c9acc56cf5b3511f3292e6e6be82
pks121 <pks667553@gmail.com>
2023-07-18 03:05:34 +0000

    moved environment outside app

--- angularapp/src/app/components/chatbot/chatbot.component.ts
+++ angularapp/src/app/components/chatbot/chatbot.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { environment } from 'src/app/environment';
+import { environment } from 'src/environment';
 
 @Component({
   selector: 'app-chatbot',
