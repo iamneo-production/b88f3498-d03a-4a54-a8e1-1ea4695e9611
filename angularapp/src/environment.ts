527c1f7000f11bd6ddc725073074cde79e1cb007
mathushree <mathushree55@gmail.com>
2023-07-24 18:32:01 +0000

    updated CORS configuration.. now all working

--- angularapp/src/environment.ts
+++ angularapp/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   API_KEY: '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',
-  baseUrl: 'https://8080-bfbbcbbafccbbbdaaaccdcddcffebdffccbebc.project.examly.io',
+  baseUrl: 'https://8080-cedbefdfddfcebbdaaaccdcddcffebdffccbebc.project.examly.io',
 };
