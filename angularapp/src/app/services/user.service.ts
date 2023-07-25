8dfc7370967a02362c486b712e6d3cf2f71af6ab
pks121 <pks667553@gmail.com>
2023-07-25 16:41:05 +0000

    Added a workout request in userservice.ts + customNutrition is connected to database

--- angularapp/src/app/services/user.service.ts
+++ angularapp/src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable, OnDestroy } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environment';
@@ -8,15 +8,11 @@ import { TokenService } from './token.service';
   providedIn: 'root'
 })
 export class UserService implements OnDestroy {
-  private httpClient: HttpClient;
-  private tokenService: TokenService;
+  constructor(private http: HttpClient, private tokenService: TokenService) {
 
-  constructor(httpClient: HttpClient, tokenService: TokenService) {
-    this.httpClient = httpClient;
-    this.tokenService = tokenService;
   }
 
-  async getCurrentUser() {
+ async getCurrentUser() {
     let activeUser = this.user;
     console.log("tokrn in userService", this.tokenService.getToken());
     
@@ -31,8 +27,8 @@ export class UserService implements OnDestroy {
     return activeUser;
   }
 
-  baseUrl:string = environment.baseUrl;
-  user: any = {id:0, email: '',  name: 'DefaultUser', password: '', height: 67, weight: 56, age: 20, gender: 'Female', imagePath:  "./../../../assets/icon/user.png" };
+  baseUrl: string = environment.baseUrl;
+  user: any = { email: '', name: 'DefaultUser', password: '', height: 67, weight: 56, age: 20, gender: 'Female', imagePath: "./../../../assets/icon/user.png" };
   userCalorie: number = 2000;
   userSubject = new BehaviorSubject(this.user);
 
@@ -44,13 +40,23 @@ export class UserService implements OnDestroy {
     this.userSubject.next(user);
   }
   getUser() {
-    this.userSubject.subscribe(newUser =>{
+    this.userSubject.subscribe(newUser => {
       this.user = newUser;
     });
     return this.user;
   }
 
 
+  getUserWorkout(){
+    let workouts:any = [];
+    this.http.get(`${this.baseUrl}/workout/userId`, { headers: {'Authorization': 'Bearer ' + this.tokenService.getToken() }, params : {'userId': this.getUser().id} }).subscribe({
+      next: (workout:any)=>{
+        workouts = workout;
+      }
+    })
+    return workouts;
+  
+  }
   getUserCalorie(weight: number) {
     let BMR = 0;
     let activity_factor = 1.2;
@@ -62,13 +68,13 @@ export class UserService implements OnDestroy {
         BMR = (10 * weight) + (6.25 * userData.height) - (5 * userData.age) - 161;
       }
       // getting activity_factor if one workout allowed perday
-      if (userData.workouts.length > 0 && userData.workouts.length <= 3)
+      if (this.getUserWorkout() && (this.getUserWorkout().length > 0 && this.getUserWorkout().length <= 3))
         activity_factor = 1.375;
-      else if (userData.workouts.length > 3 && userData.workouts.length <= 5)
+      else if (this.getUserWorkout().length > 3 && this.getUserWorkout().length <= 5)
         activity_factor = 1.55;
-      else if (userData.workouts.length > 5 && userData.workouts.length <= 7)
+      else if (this.getUserWorkout().length > 5 && this.getUserWorkout().length <= 7)
         activity_factor = 1.725;
-      else if (userData.workouts.length > 7)
+      else if (this.getUserWorkout().length > 7)
         activity_factor = 1.9;
       // calorie calculation
     })
--- angularapp/src/environment.ts
+++ angularapp/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_KEY: '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',
+  API_KEY: "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",
   baseUrl: 'https://8080-bfbbcbbafccbbbdaaaccdcddcffebdffccbebc.project.examly.io',
 };
