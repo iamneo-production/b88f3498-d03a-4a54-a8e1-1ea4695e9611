e5eaf598cf5f89423a1333cea681f1e44369472a
pks121 <pks667553@gmail.com>
2023-07-26 07:58:03 +0000

    workout history delete methods correction + recommendation page connected with backend

--- springapp/src/main/java/com/example/springapp/recommendedFood/recommendedFoodController.java
+++ springapp/src/main/java/com/example/springapp/recommendedFood/recommendedFoodController.java
@@ -1,21 +1,27 @@
 package com.example.springapp.recommendedFood;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/nutrition")
+@Transactional
 public class recommendedFoodController {
 
     @Autowired
     private recommendedFoodRepository foodRepository;
 
-    @PostMapping("/addNutrition/{userId}")
+    @PutMapping("/addNutrition/{userId}")
     public ResponseEntity<String> saveFoodData(@PathVariable("userId") long userId,
             @RequestBody RecommendedFoodModel foodBody) {
         foodBody.setUserId(userId);
@@ -27,6 +33,13 @@ public class recommendedFoodController {
     public Iterable<RecommendedFoodModel> getAllNutritionData(@PathVariable("userId") long userId) {
         return foodRepository.findAllByUserId(userId);
 
+    }
+
+    @DeleteMapping("/deleteNutrition/{userId}")
+    public Iterable<RecommendedFoodModel> deleteAllNutritions(@PathVariable("userId") long userId){
+        foodRepository.deleteAllByUserId(userId);
+        Iterable<RecommendedFoodModel> newNutrition = foodRepository.findAllByUserId(userId);
+        return newNutrition;
     }
 
 }
