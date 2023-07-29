99c5eaeff8ccee9310ba9a44b0c412ccc9350323
kiran-kumar-reddy <bijivemulakiran8@gmail.com>
2023-07-29 09:10:30 +0000

    #

--- angularapp/src/environment.ts
+++ angularapp/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_KEY: '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',
+  API_KEY: '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',
   baseUrl: 'https://8080-cddcfabcbdedaaaccdcddcffebeaeaadbdbabf.project.examly.io'
 };
